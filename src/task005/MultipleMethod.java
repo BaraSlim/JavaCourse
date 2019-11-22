46447f2a3f16994817eab521f2c11277f1888e8c
BaraSlim <painkiller4you@yandex.ru>
2019-11-22 17:43:48 +0300

    great optimization

--- src/task005/MultipleMethod.java
+++ src/task005/MultipleMethod.java
@@ -2,8 +2,7 @@ package task005;
 
 public class MultipleMethod {
     public static void multipleMethod(int number, int i) {
-        if (i < 10) {
-            ++i;
+        if (i++ < 10) {
             System.out.println(i + " x " + number + " = " + number * i);
             multipleMethod(number, i);
         }
