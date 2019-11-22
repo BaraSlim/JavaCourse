7e32001d1a6b0606b86e503f597a90b467742db9
BaraSlim <painkiller4you@yandex.ru>
2019-11-23 00:55:40 +0300

    AverageCycle

--- src/task006/AverageCycle.java
+++ src/task006/AverageCycle.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class AverageCycle {
     public static void main(String[] args) {
-        int x, y, z;
+        int x, y, z, average;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число для X : ");
         x = scanner.nextInt();
@@ -12,10 +12,11 @@ public class AverageCycle {
         y = scanner.nextInt();
         System.out.println("Введите число для Z : ");
         z = scanner.nextInt();
-        int average = x*y*z/3;
-        System.out.println("Среднее арифметическое число = " +average);
-
-
-
+        average = x * y * z / 3;
+        System.out.println("Полученные цифры: \nX = " + x + "\nY = " + y + "\nZ = " + z);
+        System.out.println("Среднее арифметическое число = " + average);
+        if ((average % 2) == 0) {
+            System.out.println("Программа выполнена корректно");
+        }
     }
 }
