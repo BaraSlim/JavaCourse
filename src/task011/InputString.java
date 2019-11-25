d6487abad7e900ef3f62cd50250550d5980321b6
BaraSlim <painkiller4you@yandex.ru>
2019-11-25 10:43:09 +0300

    InputString #1Commit: -delete string variable

--- src/task011/InputString.java
+++ src/task011/InputString.java
@@ -4,10 +4,9 @@ import java.util.Scanner;
 
 public class InputString {
     public static void main(String[] args) {
-        String userString;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите Ваше послание ");
-        userString = scanner.nextLine();
+        String userString = scanner.nextLine();
         System.out.println("Ваше послание '" +userString+ "' отправлено");
     }
 }
