0b067bb43c0e86652796f003cdd69428ae97f7d8
BaraSlim <painkiller4you@yandex.ru>
2019-12-04 15:59:30 +0300

    AddSome

--- src/task018/UserTXTRewrite.java
+++ src/task018/UserTXTRewrite.java
@@ -1,15 +1,15 @@
 package task018;
 
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-import org.w3c.dom.ls.LSOutput;
-
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+/**
+ * Необходимо написать программу, которая будет выводить на экран текстовые данные из файла .txt,
+ * а потом в этот же файл перезаписывать текстовые данные, введенные вручную.
+ * Количество строк после перезаписи должно быть столько же, сколько и в изначальном варианте.
+ */
 
 public class UserTXTRewrite {
     public static void main(String[] args) {
@@ -29,29 +29,29 @@ public class UserTXTRewrite {
         }
         String[] array = list.toArray(new String[0]);      //добавить(toArray) в массив array[] все считанные строки из файла list с типом String[]
         in.close();
+
         /*=========================Метод изменения содержимого массива=========================*/
-        System.out.println("Введите номер записи для изменения: от 0 до "+array.length);
+        System.out.println("Введите номер записи для изменения: от 0 до " + array.length);
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
-        System.out.println("Оригинальная запись из файла = \n"+array[num]);
+        System.out.println("Оригинальная запись из файла = \n" + array[num] + "\n");
         System.out.println("Введите альтернативный текст для записи № " + num);
-        String newUserString = scanner.nextLine();
-        System.out.println("Альтернативная запись = \n" + newUserString);
-        array[num]=newUserString;
+        Scanner scanner2 = new Scanner(System.in);
+        String newUserString = scanner2.nextLine();
+        System.out.println("Запись в файле обновлена");
+        array[num] = newUserString;
 
         /*=========================Метод сохранения содержимого массива в файл .txt=========================*/
-
-        try (OutputStream output = Files.newOutputStream(Paths.get(path),  StandardOpenOption.CREATE_NEW);
-             BufferedOutputStream outputStream = new BufferedOutputStream(output)) {
-            for (int i = 0; i < array.length ; i++) {
-                outputStream.write(array[i].getBytes());
-            }
-        } catch (IOException e) {
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter(path);
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        for (int i = 0; i < array.length; i++) {
+            writer.println(array[i]);
+        }
+        writer.close();
+        System.out.println("Спасибо! Обновленный документ доступен по адресу.\nКоличество строк в файле = " + array.length);
     }
 }
-
-//    Необходимо написать программу, которая будет выводить на экран текстовые данные из файла .txt,
-//        а потом в этот же файл перезаписывать текстовые данные, введенные вручную.
-//        Количество строк после перезаписи должно быть столько же, сколько и в изначальном варианте.
